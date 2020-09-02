e05a57dabe5e2357a21635475cbe57b5992b67c6
CookieCrumbs19212 <62071086+CookieCrumbs19212@users.noreply.github.com>
2020-09-02 20:40:05 +0400

    Update Cell.java
    converted all bytes to ints

--- java/com/yde/solvadoku/Cell.java
+++ java/com/yde/solvadoku/Cell.java
@@ -7,94 +7,97 @@ import java.util.Arrays;
 /**
  * An instance of the Cell class is used to represent one cell in the sudoku
  * puzzle.It consists of an array (candidates) that contains a list of the
- * possible candidates that a specific cell can have. It also consists of a byte
+ * possible candidates that a specific cell can have. It also consists of a int
  * member variable (solution) that is meant to represent the final inserted
  * number into the cell.
  */
 public class Cell {
 
     private final boolean[] candidates;
-    private byte solution;
-    private byte noOfCandidates;
+    private int solution;
+    private int noOfCandidates;
 
     /**
-     * initializes array possibilities true for all bytes from 1 to 9, indicating
+     * initializes array possibilities true for all integers from 1 to 9, indicating
      * that all numbers are candidates initially.
      */
     public Cell() {
         candidates = new boolean[9];
-        for (byte i = 0; i < 9; i++)
+        for (int i = 0; i < 9; i++)
             candidates[i] = true;
 
         noOfCandidates = 9;
         solution = 0;
     }// end of constructor
 
-    /**
-     * provides the number of candidates for the cell
+    /** 
+     * returns the number of candidates for the cell 
      */
-    public byte getNoOfCandidates() {
-        return (byte) noOfCandidates;
-    }// end of byte getNoOfCandidates()
+    public int getNoOfCandidates() {
+        return (int)noOfCandidates;
+    }// end of int getNoOfCandidates()
 
-    /**
-     * checks if the input byte is a current candidate in the cell
+    /** 
+     * checks if the input integer is a current candidate in the cell 
      */
-    public boolean isCandidate(byte input) {
-        return candidates[input - 1];
-    }// end of boolean isCandidate(byte)
+    public boolean isCandidate(int input) {
+        return candidates[input-1];
+    }// end of boolean isCandidate(int)
 
-    /**
-     * returns the current Candidate at the entered position.
+    /** 
+     * returns the current Candidate at the entered position. 
      */
-    public byte getCandidate(int position) {
+    public int getCandidate(int position) {
         int current_pos = -1;
-        for (byte i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             if (candidates[i]) {
                 current_pos++;
                 if (current_pos == position)
-                    return (byte) (i + 1);
-            }
+                    return (int)(i + 1);           
+            }            
         }
         return -1;
-    }// end of byte getCandidate(int)
+    }// end of int getCandidate(int)
 
-    /**
-     * removes the corresponding candidate to the byte input
+    /** 
+     * removes the corresponding candidate to the int input 
      */
-    public void removeCandidate(byte input) {
-        candidates[input - 1] = false;
+    public void removeCandidate(int input) {
+        candidates[input-1] = false;
         noOfCandidates--;
-    }// end of void removeCandidate(byte)
+    }// end of void removeCandidate(int)
 
-    /**
-     * checks if the input cell has the same Candidates as the current cell
+    /** 
+     * checks if the input cell has the same Candidates as the current cell 
      */
     public boolean sameCandidates(Cell other) {
-        return (Arrays.equals(this.candidates, other.candidates));
+        return (Arrays.equals(this.candidates, other.candidates));            
     }// end of boolean sameCandidate(Cell)
 
-    /**
-     * inserts the byte input to the cell permanently
+    /** 
+     * inserts the int input to the cell permanently 
      */
-    public void placeSolution(byte input) { // a final decision is made irrespective of the current number of possibilities and Candidates
+    public void placeSolution(int input) { // a final decision is made irrespective of the current number of possibilities and Candidates
         solution = input;
-    }// end of void placeSolution(byte)
+    }// end of void placeSolution(int)
 
-    /**
-     * gets the inserted value
+    /** 
+     * gets the inserted value 
      */
-    public byte getSolution() {
+    public int getSolution() {
         return solution;
-    }// end of byte getSolution()
+    }// end of int getSolution()
 
-    /**
-     * checks if the cell has a value permanently inserted in it or not.
+    /** 
+     * checks if the cell has a value permanently inserted in it or not. 
      */
     public boolean isNotSet() {
         return (solution == 0);
     }// end of boolean isNotSet()
 
+    /** 
+     * returns list of the candidates for the cell 
+     */
     public String listCandidates() {
         String s = "";
         for (byte i = 0; i < 9; i++) {
@@ -108,8 +111,9 @@ public class Cell {
                 s += " ";
         }
         return s;
-    }
+    }// end of String listCandidates()
 
     // public int pencilMarks() {
     //}
+
 }// end of class Cell
