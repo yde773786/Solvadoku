8680267cae06a7897b1a1253e09ef7392eeaf731
Nikhil Sreekumar <63968183+yde773786@users.noreply.github.com>
2020-09-25 03:38:04 +0400

    Update 1.1 final commit
    Signed-off-by: yde773786 <yde773786@gmail.com>

--- java/com/yde/solvadoku/SettingsActivity.java
+++ java/com/yde/solvadoku/SettingsActivity.java
@@ -3,19 +3,34 @@ package com.yde.solvadoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
     Switch switcher;
     boolean isChecked;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ImageView github = findViewById(R.id.github);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                String url = (String) view.getTag();
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
         isChecked = getIntent().getBooleanExtra("input", true);
         switcher = findViewById(R.id.switcher);
         switcher.setChecked(isChecked);
@@ -23,7 +38,7 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checker) {
                 isChecked = checker;
-                Intent intent = new Intent();
+                intent = new Intent();
                 intent.putExtra("key", isChecked);
                 setResult(RESULT_OK, intent);
             }
