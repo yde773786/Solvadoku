9839e7064c99a300340a55bc5da5718decbc7008
yde773786 <yde773786@gmail.com>
2021-08-27 09:34:19 -0400

    Replaced SquareGridLayout.java with SudoduGrid.java for reusability of puzzle layout
    Signed-off-by: yde773786 <yde773786@gmail.com>

--- java/com/yde/solvadoku/SudokuGrid.java
+++ java/com/yde/solvadoku/SudokuGrid.java
@@ -2,23 +2,27 @@ package com.yde.solvadoku;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.GridLayout;
 
-import androidx.gridlayout.widget.GridLayout;
+public class SudokuGrid extends GridLayout {
 
-
-public class SquareGridLayout extends GridLayout {
-
-
-    public SquareGridLayout(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-    public SquareGridLayout(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public SquareGridLayout(Context context) {
+    public SudokuGrid(Context context) {
         super(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.sudoku_grid, this);
+    }
+
+    public SudokuGrid(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.sudoku_grid, this);
+    }
+
+    public SudokuGrid(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.sudoku_grid, this);
     }
 
     @Override
@@ -26,6 +30,7 @@ public class SquareGridLayout extends GridLayout {
         int width = MeasureSpec.getSize(widthSpec);
         int height = MeasureSpec.getSize(heightSpec);
         int makeSquare = Math.min(width, height);
+
         if ((double) width / height <= 0.66) {
             super.onMeasure(MeasureSpec.makeMeasureSpec(makeSquare, MeasureSpec.EXACTLY),
                     MeasureSpec.makeMeasureSpec(makeSquare, MeasureSpec.EXACTLY));
