68fad1da9d024515ab4da3abd0c31b74c3c4c420
yde773786 <yde773786@gmail.com>
2021-09-05 02:14:20 -0400

    Error validation complete
    Signed-off-by: yde773786 <yde773786@gmail.com>

--- main/java/com/yde/solvadoku/UI/Grids/SudokuGrid.java
+++ main/java/com/yde/solvadoku/UI/Grids/SudokuGrid.java
@@ -12,13 +12,17 @@ import androidx.gridlayout.widget.GridLayout;
 import com.yde.solvadoku.R;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class SudokuGrid extends SquareGrid {
 
     private HashMap<TextView, int[]> cellToState = new HashMap<>();
     private TextView[][] unit;
-    private final int CLEAR = 0, DISABLED = 1, SELECTED = 2, INVALID = 3;
+    private boolean isLegalPuzzle;
+    private boolean[][] isError;
+    private final int CLEAR = 0, DISABLED = 1, SELECTED = 2, INVALID = 3, CURRENT = 4;
     private TextView focusedCell;
 
     public SudokuGrid(Context context) {
@@ -51,10 +55,10 @@ public class SudokuGrid extends SquareGrid {
                     @Override
                     public void onClick(View view) {
                         if (focusedCell != null) {
-                            focusedCell.setBackgroundResource(Objects.requireNonNull(cellToState.get(focusedCell))[CLEAR]);
+                            switchBackground(focusedCell, getCurrentState(focusedCell));
                         }
                         focusedCell = textView;
-                        textView.setBackgroundResource(Objects.requireNonNull(cellToState.get(textView))[SELECTED]);
+                        switchBackground(textView, SELECTED);
                     }
                 });
 
@@ -89,42 +93,43 @@ public class SudokuGrid extends SquareGrid {
         if (c == 2 || c == 5) {
             if (r == 2 || r == 5) {
                 cellToState.put(textView, new int[]{R.drawable.bottom_right, R.drawable.bottom_rightgrey,
-                        R.drawable.bottom_rightblue, R.drawable.bottom_rightred});
+                        R.drawable.bottom_rightblue, R.drawable.bottom_rightred, 0});
             } else if (r == 3 || r == 6) {
                 cellToState.put(textView, new int[]{R.drawable.top_right, R.drawable.top_rightgrey,
-                        R.drawable.top_rightblue, R.drawable.top_rightred});
+                        R.drawable.top_rightblue, R.drawable.top_rightred, 0});
             } else
                 cellToState.put(textView, new int[]{R.drawable.right_border, R.drawable.right_bordergrey,
-                        R.drawable.right_borderblue, R.drawable.right_borderred});
+                        R.drawable.right_borderblue, R.drawable.right_borderred, 0});
         } else if (c == 3 || c == 6) {
             if (r == 2 || r == 5) {
                 cellToState.put(textView, new int[]{R.drawable.bottom_left, R.drawable.bottom_leftgrey,
-                        R.drawable.bottom_leftblue, R.drawable.bottom_leftred});
+                        R.drawable.bottom_leftblue, R.drawable.bottom_leftred, 0});
             } else if (r == 3 || r == 6) {
                 cellToState.put(textView, new int[]{R.drawable.top_left, R.drawable.top_leftgrey,
-                        R.drawable.top_leftblue, R.drawable.top_leftred});
+                        R.drawable.top_leftblue, R.drawable.top_leftred, 0});
             } else
                 cellToState.put(textView, new int[]{R.drawable.left_border, R.drawable.left_bordergrey,
-                        R.drawable.left_borderblue, R.drawable.left_borderred});
+                        R.drawable.left_borderblue, R.drawable.left_borderred, 0});
         } else if (r == 2 || r == 5) {
             cellToState.put(textView, new int[]{R.drawable.bottom_border, R.drawable.bottom_bordergrey,
-                    R.drawable.bottom_borderblue, R.drawable.bottom_borderred});
+                    R.drawable.bottom_borderblue, R.drawable.bottom_borderred, 0});
         } else if (r == 3 || r == 6) {
             cellToState.put(textView, new int[]{R.drawable.top_border, R.drawable.top_bordergrey,
-                    R.drawable.top_borderblue, R.drawable.top_borderred});
+                    R.drawable.top_borderblue, R.drawable.top_borderred, 0});
         } else
             cellToState.put(textView, new int[]{R.drawable.border, R.drawable.bordergrey,
-                    R.drawable.borderblue, R.drawable.borderred});
+                    R.drawable.borderblue, R.drawable.borderred, 0});
     }
 
     /**
-     * Set the focused cel as value inputted
+     * Set the focused cell as value inputted.
      *
      * @param value Value of focused cell required
      */
     public void setFocusedValue(String value) {
         if (focusedCell != null) {
             focusedCell.setText(value);
+            paintErrors();
         }
     }
 
@@ -135,6 +140,127 @@ public class SudokuGrid extends SquareGrid {
      */
     public TextView[][] getUnit() {
         return unit;
+    }
+
+    /**
+     * Paints the row, column or block with invalid input, if any.
+     */
+    private void paintErrors() {
+        isLegalPuzzle = true;
+        isError = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            validateBlock(i);
+            validateRow(i);
+            validateColumn(i);
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (isError[i][j]) {
+                    switchBackground(unit[i][j], INVALID);
+                } else {
+                    switchBackground(unit[i][j], CLEAR);
+                }
+            }
+        }
+    }
+
+    /**
+     * Find if erroneous input is present in column and update cell
+     * error status as required.
+     *
+     * @param i Index of column
+     */
+    private void validateColumn(int i) {
+        Set<Integer> repeat = new HashSet<>();
+
+        for (int j = 0; j < 9; j++) {
+            Integer currValue = unit[j][i].getText().toString().equals("") ? 0 : Integer.parseInt(unit[j][i].getText().toString());
+            if (repeat.contains(currValue) && currValue != 0) {
+                for (int k = 0; k < 9; k++) {
+                    isError[k][i] = true;
+                }
+                isLegalPuzzle = false;
+                return;
+            }
+            repeat.add(currValue);
+        }
+    }
+
+    /**
+     * Find if erroneous input is present in row and update cell
+     * error status as required.
+     *
+     * @param i Index of row
+     */
+    private void validateRow(int i) {
+        Set<Integer> repeat = new HashSet<>();
+
+        for (int j = 0; j < 9; j++) {
+            Integer currValue = unit[i][j].getText().toString().equals("") ? 0 : Integer.parseInt(unit[i][j].getText().toString());
+            if (repeat.contains(currValue) && currValue != 0) {
+                for (int k = 0; k < 9; k++) {
+                    isError[i][k] = true;
+                }
+                isLegalPuzzle = false;
+                return;
+            }
+            repeat.add(currValue);
+        }
+    }
+
+    /**
+     * Find if erroneous input is present in block and update cell
+     * error status as required.
+     *
+     * @param i Index of block
+     */
+    private void validateBlock(int i) {
+        int startI = 3 * (i / 3);
+        int startJ = 3 * (i % 3);
+        Set<Integer> repeat = new HashSet<>();
+
+        for (int k = startI; k < startI + 3; k++) {
+            for (int l = startJ; l < startJ + 3; l++) {
+                Integer currValue = unit[k][l].getText().toString().equals("") ? 0 : Integer.parseInt(unit[k][l].getText().toString());
+
+                if (repeat.contains(currValue) && currValue != 0) {
+                    for (int m = startI; m < startI + 3; m++) {
+                        for (int n = startJ; n < startJ + 3; n++) {
+                            isError[m][n] = true;
+                        }
+                    }
+                    isLegalPuzzle = false;
+                    return;
+                }
+
+                repeat.add(currValue);
+            }
+        }
+    }
+
+    /**
+     * Switches the background of cell and updates its state
+     *
+     * @param textView Cell required
+     * @param state    state to be updated to (doesn't update if selected)
+     */
+    private void switchBackground(TextView textView, int state) {
+        textView.setBackgroundResource(Objects.requireNonNull(cellToState.get(textView))[state]);
+        if (state != SELECTED) {
+            Objects.requireNonNull(cellToState.get(textView))[CURRENT] = state;
+        }
+    }
+
+    /**
+     * Gets the current state of the cell
+     *
+     * @param textView Cell required
+     * @return state cell is in currently
+     */
+    private int getCurrentState(TextView textView) {
+        return Objects.requireNonNull(cellToState.get(textView))[CURRENT];
     }
 
 }
