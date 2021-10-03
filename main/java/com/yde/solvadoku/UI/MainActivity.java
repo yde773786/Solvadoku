c2a1cfa0a030b8f1e7e28364c94212005625cfaa
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-10-04 03:07:31 +0400

    Updated

--- main/java/com/yde/solvadoku/UI/MainActivity.java
+++ main/java/com/yde/solvadoku/UI/MainActivity.java
@@ -57,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         try {
