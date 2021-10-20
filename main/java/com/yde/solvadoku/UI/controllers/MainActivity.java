665b2bd618edd4fceefa4469cf6c7312cd3616ba
yde773786 <yde773786@gmail.com>
2021-10-20 12:18:58 -0400

    Unused Import
    Signed-off-by: yde773786 <yde773786@gmail.com>

--- main/java/com/yde/solvadoku/UI/controllers/MainActivity.java
+++ main/java/com/yde/solvadoku/UI/controllers/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
